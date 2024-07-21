d71845fea5fdf5a166734b2cfb3c7d668ad130fc
Robert Rasmussen <bobbyras90@gmail.com>
2024-07-20 23:31:00 -0700

    Remove bevy_inspector_egui

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,13 +1,12 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
 use bevy::{dev_tools::states::log_transitions, prelude::*};
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::{game::state::GameState, screen::Screen};
 
 pub(super) fn plugin(app: &mut App) {
     // Add bevy-inspector-egui
-    app.add_plugins(WorldInspectorPlugin::new());
+    // app.add_plugins(bevy_inspector_egui::WorldInspectorPlugin::new());
 
     // Print state transitions in dev builds
     app.add_systems(Update, log_transitions::<Screen>);
